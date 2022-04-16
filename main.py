d9cc3954416ac5e07eb595047d699eaefb1c384c
kevingq <kevin.gracequist@gmail.com>
2022-04-16 17:58:44 -0400

    Fixed a bug that caused particle collision where the boundary was supposed to have ended

--- main.py
+++ main.py
@@ -3,20 +3,22 @@ from Boundary import Boundary
 import numpy as np
 sim = Simulation(0)
 # Set simulation parameters (overwrite default)
-sim.setInitialPosition(np.array([0.0,0.0]))
-sim.setInitialVelocity(np.array([2.0,2.0]))
-sim.setInitialAcceleration(np.array([0.0,-9.81]))
 sim.setStepTime(0.03)
-sim.setWorldSize([[-2.0, 4.0],[-2.0,2.0]])
+sim.setWorldSize([[-7.0, 7.0],[-3.0,2.0]])
+sim.setInitialPosition(np.array([1.2,-0.2]))
+sim.setInitialVelocity(np.array([2.0,-2.0]))
+sim.setInitialAcceleration(np.array([0.0,-9.81]))
 sim.setStartTime(0.0)
-sim.setStopTime(3.0)
+sim.setStopTime(7.0)
 
 # Define any walls in the world
-b1=Boundary('b1','line',np.array([[-2.0,1.0],[-1.0,-1.0]]))
-b2=Boundary('b1','line',np.array([[1.0,3.0],[-1.0,0.0]]))
+
+b1=Boundary('b1','line',np.array([[1.0,3.0],[-1.0,0.0]]))
+b2=Boundary('b2','line',np.array([[0.0,1.0],[-0.5,0.5]]))
+b3=Boundary('b3','line',np.array([[-7.0,7.0],[-1.5,-1.5]]))
 
 # Create array of boundaries and add them to the simulation
-boundaries = np.array([b1,b2])
+boundaries = np.array([b1,b2,b3])
 sim.setBoundaries(boundaries)
 
 # Start the simulation
