13a6be861a00dd6d212746feb17e2fe919eba79f
kevingq <kevin.gracequist@gmail.com>
2022-04-17 23:26:49 -0400

    Still trying to fix author issues, git is confusing

--- Particle.py
+++ Particle.py
@@ -21,7 +21,6 @@ class Particle:
     def getAcc(self):
         return self.aVec
 
-<<<<<<< HEAD
     def normal(self,v):
         dir_y=(v[1][1]-v[1][0])
         dir_x=(v[0][1]-v[0][0])
@@ -29,8 +28,6 @@ class Particle:
         perp_dir=(1/np.linalg.norm(perp_dir))*perp_dir # set magnitude to 1
         return perp_dir
 
-=======
->>>>>>> 811cd89... Simulation checks all boundaries for particle each step
     def standardForm(self,line):
         x1=line[0][0]
         x2=line[0][1]
@@ -46,10 +43,8 @@ class Particle:
         bx2=boundary[0][1]
         by1=boundary[1][0]
         by2=boundary[1][1]
-<<<<<<< HEAD
-=======
+
         tan_bound="Normal vector to collision: Not yet implemented"
->>>>>>> 811cd89... Simulation checks all boundaries for particle each step
         px1=self.pVec[0]
         py1=self.pVec[1]
         px2=pVec_pred[0]
@@ -66,7 +61,6 @@ class Particle:
             distP2_to_I=np.linalg.norm(np.array([[x[0]-px2],[x[1]-py2]]))
             distP1_to_P2=np.linalg.norm(np.array([[px1-px2],[py1-py2]]))
             minDistToBoundary=np.linalg.norm((distP1_to_P2 - (distP1_to_I+distP2_to_I)))
-<<<<<<< HEAD
 
             if minDistToBoundary < 0.08:
                 return [True,x]
@@ -74,16 +68,12 @@ class Particle:
                 return [False,x]
         except :
             return [False,x]
-=======
             if minDistToBoundary < 0.08:
                 return [True,tan_bound]
             else:
                 return [False,tan_bound]
         except :
             return [False,tan_bound]
->>>>>>> 811cd89... Simulation checks all boundaries for particle each step
-
-
 
     def takeStep(self,tStep,boundaries):
         # Fixme: Test if vector [pVec_pred - pVec] crosses any boundaries
@@ -94,15 +84,12 @@ class Particle:
             collision=self.willCollide(pVec_pred,b)
             collisionFound=collision[0]
             if collisionFound:
-<<<<<<< HEAD
                 xI=collision[1][0]
                 yI=collision[1][1]
                 n=self.normal(b)
                 self.vVec=n*np.linalg.norm(vVec_pred)
-=======
                 print(collision[1])
                 self.vVec=-vVec_pred
->>>>>>> 811cd89... Simulation checks all boundaries for particle each step
                 self.pVec=np.add(self.pVec,self.vVec*tStep)
                 return True
         self.vVec=vVec_pred
